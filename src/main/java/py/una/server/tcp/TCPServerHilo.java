7f479802ab2fffeff8fcfdcec915ea19b5d26d8c
Alberto Fernandez <albertofer97@gmail.com>
2020-09-23 21:34:12 -0400

    validaciones de disponibilidad

--- src/main/java/py/una/server/tcp/TCPServerHilo.java
+++ src/main/java/py/una/server/tcp/TCPServerHilo.java
@@ -32,7 +32,7 @@ public class TCPServerHilo extends Thread {
 			while (true) {
 				Persona cliente = PersonaJSON.stringObjeto(in.readLine());
 				int operacion = cliente.getOperacion();
-				//System.out.println("Operacion recibida: " + operacion);
+				// System.out.println("Operacion recibida: " + operacion);
 
 				if (operacion == 0) {
 					servidor.usuarios.add(cliente);
@@ -40,11 +40,11 @@ public class TCPServerHilo extends Thread {
 					servidor.clientes.put(cliente.getId(), cliente);
 					outputLine = "Usuario/a " + cliente.getNombre() + " agregado";
 				} else if (operacion == 2) {
-					
+
 					for (Long key : servidor.clientes.keySet()) {
 						String name = servidor.clientes.get(key).getNombre();
 						if (name.equals(cliente.getDestino())) {
-							personaDestino = servidor.clientes.get(key); 
+							personaDestino = servidor.clientes.get(key);
 							servidor.clientes.get(key).setDestino(cliente.getNombre());
 							cliente.setDestino(personaDestino.getNombre());
 							cliente.setSocket(this.socket);
@@ -52,46 +52,68 @@ public class TCPServerHilo extends Thread {
 							break;
 						}
 					}
-					Socket socketDestino= personaDestino.getSocket();
-					PrintWriter out2 = new PrintWriter(socketDestino.getOutputStream(), true);
-					while(true) {
-						mensajeEnviado=in.readLine();
-						//System.out.println("Mensaje recibido de "+cliente.getNombre()+" con el mensaje "+mensajeEnviado);
-						if(mensajeEnviado.equals("chau")) {
-							break;
-						}
-						out2.println(mensajeEnviado);
-						//bw.flush();
+					if (personaDestino.getDisponible()) {
 						
+						out.println("disponible");
+						cliente.setSocket(this.socket);
+						cliente.setDisponible(false);
+						servidor.clientes.put(cliente.getId(), cliente);
+						
+						Socket socketDestino = personaDestino.getSocket();
+						PrintWriter out2 = new PrintWriter(socketDestino.getOutputStream(), true);
+						while (true) {
+							mensajeEnviado = in.readLine();
+							// System.out.println("Mensaje recibido de "+cliente.getNombre()+" con el
+							// mensaje "+mensajeEnviado);
+							if (mensajeEnviado.equals("chau")) {
+								break;
+							}
+							out2.println(mensajeEnviado);
+							// bw.flush();
+
+						}
+
+						cliente.setDisponible(true);
+						cliente.setSocket(this.socket);
+						servidor.clientes.put(cliente.getId(), cliente);
+					}else {
+						out.println("no disponible");
 					}
-					
-					
+
 				} else if (operacion == 3) {
-					//String nombreOrigen="";
-					Persona origen= new Persona();
+					// String nombreOrigen="";
+					cliente.setDisponible(false);
+					cliente.setSocket(this.socket);
+					servidor.clientes.put(cliente.getId(), cliente);
+
+					Persona origen = new Persona();
 					for (Long key : servidor.clientes.keySet()) {
 						origen = servidor.clientes.get(key);
 						if (origen.getDestino().equals(cliente.getNombre())) {
-							personaDestino = servidor.clientes.get(key); 
-							//System.out.println("Se agrego el socket");
+							personaDestino = servidor.clientes.get(key);
+							// System.out.println("Se agrego el socket");
 							break;
 						}
 					}
-					
+
 					out.println(origen.getNombre());
-				
-					Socket socketDestino= personaDestino.getSocket();
+
+					Socket socketDestino = personaDestino.getSocket();
 					PrintWriter out2 = new PrintWriter(socketDestino.getOutputStream(), true);
-					while(true) {
-						mensajeEnviado=in.readLine();
-						//System.out.println("Mensaje recibido de "+cliente.getNombre()+" con el mensaje "+mensajeEnviado);
-						if(mensajeEnviado.equals("chau")) {
+					while (true) {
+						mensajeEnviado = in.readLine();
+						// System.out.println("Mensaje recibido de "+cliente.getNombre()+" con el
+						// mensaje "+mensajeEnviado);
+						if (mensajeEnviado.equals("chau")) {
 							break;
 						}
 						out2.println(mensajeEnviado);
 					}
-					
-					
+
+					cliente.setDisponible(true);
+					cliente.setSocket(this.socket);
+					servidor.clientes.put(cliente.getId(), cliente);
+
 				} else if (operacion == 4) {
 					servidor.listening = false;
 					outputLine = "Usted apago todo";
@@ -107,19 +129,23 @@ public class TCPServerHilo extends Thread {
 
 				} else if (operacion == 1) {
 
-					outputLine = "Lista de usuarios: ";
+					outputLine = "Lista de usuarios disponibles: ";
 
 					Iterator<Persona> iter = servidor.usuarios.iterator();
 
 					while (iter.hasNext()) {
-						outputLine = outputLine + " - " + iter.next().getNombre();
+						Persona p = iter.next();
+						if (p.getDisponible() && (p.getNombre().equals(cliente.getNombre())) == false) {
+							System.out.println("persona" +p.getNombre()+" disponible: "+p.getDisponible());
+							outputLine = outputLine + " - " + p.getNombre();
+						}
 					}
 					out.println(outputLine);
 				} else {
 					System.out.println("No se reconocio la operacion");
 				}
 
-				//out.println(outputLine);
+				// out.println(outputLine);
 			}
 			out.close();
 			in.close();
